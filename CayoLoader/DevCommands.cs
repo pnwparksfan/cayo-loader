08ea3292f94c2eaa6d19b62705c330cbe379a70b
PNWParksFan <gtaparks@gmail.com>
2021-01-03 14:36:54 -0800

    Added minimap rendering

--- CayoLoader/DevCommands.cs
+++ CayoLoader/DevCommands.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace CayoLoader
 {
     using Rage;
+    using Rage.Native;
     using Rage.Attributes;
 
     internal static class DevCommands
@@ -40,6 +41,32 @@ namespace CayoLoader
             MapFunctions.ToggleIslandMap(true);
             GameFiber.Sleep(2000);
             MapFunctions.DisableIslandCullbox();
+        }
+
+        private static Blip mapBoundsBlip;
+        private static GameFiber minimapFiber;
+        [ConsoleCommand]
+        private static void EnableIslandMinimap(bool enable)
+        {
+            bool running = minimapFiber?.IsAlive == true;
+            
+            if (running && !enable) minimapFiber.Abort();
+            
+            if(!running && enable)
+            {
+                if(!mapBoundsBlip)
+                {
+                    mapBoundsBlip = new Blip(new Vector3(7000, -7000, 0));
+                    mapBoundsBlip.Scale = 0;
+                    mapBoundsBlip.Alpha = 1;
+                }
+
+                minimapFiber = GameFiber.ExecuteNewWhile(() => 
+                {
+                    NativeFunction.Natives.SET_RADAR_AS_INTERIOR_THIS_FRAME(Game.GetHashKey("h4_fake_islandx"), 4700.0f, -5145.0f, 0, 0);
+                    NativeFunction.Natives.SET_RADAR_AS_EXTERIOR_THIS_FRAME();
+                }, () => true);
+            }
         }
     }
 }
