7363a2996220821660f1261910ab38fb5360ba91
PNWParksFan <gtaparks@gmail.com>
2021-01-02 17:49:52 -0800

    Initial commit

---
+++ CayoLoader/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Rage.Attributes.Plugin("Cayo Perico Loader", Description = "Loads Cayo Perico seamlessly", Author = "PNWParksFan", ShouldTickInPauseMenu = true, PrefersSingleInstance = true)]
+
+namespace CayoLoader
+{
+    using Rage;
+    using Rage.Native;
+
+    internal static class EntryPoint
+    {
+        private static void Main()
+        {
+            Game.LogTrivial("Loaded Cayo Perico Loader");
+            GameFiber.Hibernate();
+        }
+    }
+}
